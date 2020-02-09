f938a9ca9db91952cb97f0481f9f267fc4c4141c
Angelika Cimene <angelikacimene@gmail.com>
2020-02-09 02:52:13 -0500

    Along's Portfolio Website
    init

---
+++ script.js
@@ -0,0 +1,28 @@
+// 'use strict';
+// console.log('reading js');
+//
+//
+// // Variables
+// var about = document.querySelector('#about');
+// var featured = document.querySelector('#featured');
+// var projectOne = document.querySelector('#projectOneTitle');
+// var projectTwo = document.querySelector('#projectPageTwo');
+// var projectThree = document.querySelector('#projectPageThree');
+
+
+//Pages
+// about.addEventListener('click', function() {
+//   document.querySelector('main').style.display = 'none';
+//   document.querySelector('#AboutPage').style.display = 'block';
+// });
+//
+// featured.addEventListener('click', function() {
+//   document.querySelector('main').style.display = 'none';
+//   document.querySelector('#featuredProject').style.display = 'block';
+// });
+//
+// if ( featured < 600) {
+//   featured.addEventListener('click', function() {
+//     document.querySelector('main').style.display = 'none';
+//     document.querySelector('#featuredProject').style.display = 'flex';
+// }
